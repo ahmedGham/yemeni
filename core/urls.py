3b650c9a4b7d20aae026dcfc72ca30c62f862802
ahmed <ahmed@gmail.com>
2022-08-10 01:08:19 +0300

    fix bugs

--- core/urls.py
+++ core/urls.py
@@ -8,9 +8,11 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('food/', include('food.urls')),
     path('',include('app.urls')),
-    re_path(r'^.*', TemplateView.as_view(template_name='index.html')),
+    
 ]
 
 
 urlpatterns += static(settings.MEDIA_URL,document_root = settings.MEDIA_ROOT)
 
+
+urlpatterns += [re_path(r'^.*',TemplateView.as_view(template_name='index.html'))]
